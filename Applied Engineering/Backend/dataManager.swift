8f5df0b2545f9d91a58b25325a79e051d889b05d
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-04-20 22:20:24 -0700

    Update xCode project

--- Applied Engineering/Backend/dataManager.swift
+++ Applied Engineering/Backend/dataManager.swift
@@ -8,6 +8,26 @@
 import Foundation
 import UIKit
 import SwiftyZeroMQ5
+// ["RPM", "Torque", "Throttle (%)", "Duty (%)", "PWM Frequency", "Temperature (C)", "Source Voltage", "PWM Current", "Power Change (Δ)", "Voltage Change (Δ)"];
+public struct APiData : Decodable{
+    var psuMode : Int = 0; // power supply mode - 3
+    // graphable data
+    var throttlePercent : Int = 0;
+    var dutyPercent : Int = 0;
+    var pwmFrequency : Int = 0;
+    var rpm : Float32 = 0.0;
+    var torque : Float32 = 0.0;
+    var tempC : Float32 = 0.0;
+    var sourceVoltage : Float32 = 0.0;
+    var pwmCurrent : Float32 = 0.0;
+    var powerChange : Float32 = 0.0;
+    var voltageChange : Float32 = 0.0;
+    // graphable data
+    var mddStatus : Bool = false; // minimum duty detection - 0
+    var ocpStatus : Bool = false; // over current protection - 1
+    var ovpStatus : Bool = false; // over voltage prevention - 2
+    var timeStamp : Float64 = 0.0;
+}
 
 class dataManager{
     
@@ -28,6 +48,7 @@ class dataManager{
                         print("data recv - \(data)");
                     }
                     catch{
+                        print(error);
                         if (errno != EAGAIN){
                             log.addc("Communication error - \(error) with errno \(errno) = \(communication.convertErrno(errno))");
                         }
