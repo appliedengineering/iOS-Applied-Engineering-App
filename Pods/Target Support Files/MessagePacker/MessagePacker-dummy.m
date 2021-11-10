3b7e60be00c9d793a14dd8cfe103ac742d81958a
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2021-11-10 11:49:36 -0800

    Remove all client specific files

--- Pods/Target Support Files/MessagePacker/MessagePacker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MessagePacker : NSObject
-@end
-@implementation PodsDummy_MessagePacker
-@end
--- Pods/Target Support Files/Pods-Applied Engineering/Pods-Applied Engineering-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Applied_Engineering : NSObject
-@end
-@implementation PodsDummy_Pods_Applied_Engineering
-@end
